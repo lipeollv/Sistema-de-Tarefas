100fcb40af275cf0c9c05041ba4842963a2d695a
lipeollv <filipe.oc@outlook.com>
2026-07-07 19:21:49 -0300

    fix: adiciona Launcher para corrigir inicializacao do JavaFX

---
+++ src/SistemaDeTarefasFX/src/main/java/sistema/de/tarefas/Launcher.java
@@ -0,0 +1,7 @@
+package sistema.de.tarefas;
+
+public class Launcher {
+    public static void main(String[] args) {
+        App.main(args); // Chama a classe App que contém o extends Application
+    }
+}
