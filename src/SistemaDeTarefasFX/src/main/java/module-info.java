fd477759391b9230561496cbb4210ea775b27148
lipeollv <filipe.oc@outlook.com>
2026-07-09 13:33:24 -0300

    Organizando estrutura do projeto e corrigindo gitignore

--- src/SistemaDeTarefasFX/src/main/java/module-info.java
+++ src/SistemaDeTarefasFX/src/main/java/module-info.java
@@ -1,4 +1,10 @@
 module sistema.de.tarefas {
     requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.graphics;
+
+    // Você precisa abrir o pacote para o JavaFX conseguir ler suas classes
+    opens sistema.de.tarefas to javafx.fxml;
+
     exports sistema.de.tarefas;
 }
